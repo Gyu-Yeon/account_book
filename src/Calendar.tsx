d58466f21d87913deedc0f95c4f70cd01c0ca56c
Gyu-Yeon <idsopy@nate.com>
2022-09-11 18:50:59 +0900

    10

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
 
 export function Calender() {
+  const [week, setWeek] = useState(["일", "월", "화", "수", "목", "금", "토"]);
+  // const weekList = week.map((item) => {
+  //   return <div> item </div>;
+  // });
+
   return (
     <div className="calender mt-1">
-      <div className="row"></div>
+      <div className="row1">
+        {week.map((item) => {
+          return <div>{item}</div>;
+        })}
+      </div>
     </div>
   );
 }
